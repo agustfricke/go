85ea1b76b046fb50b079e83389845579be1eb095
Agust Fricke <agustfricke@protonmail.com>
2023-05-16 23:37:02 -0300

    puzel

---
+++ puzel/FizzBuzz.go
@@ -0,0 +1,20 @@
+// Write a program that prints the numbers from 1 to 100 and for multiples of ‘3’ print “Fizz” 
+// instead of the number and for the multiples of ‘5’ print “Buzz”. 
+// For numbers which are multiples of both 3 and 5, print “FizzBuzz”.
+package main
+
+import "fmt"
+
+func main() {
+    for i := 1; i <= 100; i++ {
+        if i%3 == 0 && i%5 == 0 {
+            fmt.Println("FizzBuzz")
+        } else if i%3 == 0 {
+            fmt.Println("Fizz")
+        } else if i%5 == 0 {
+            fmt.Println("Buzz")
+        } else {
+            fmt.Println(i)
+        }
+    }
+}
